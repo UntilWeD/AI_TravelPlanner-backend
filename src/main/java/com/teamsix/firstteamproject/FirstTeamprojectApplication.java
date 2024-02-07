ec09a6ccc04fa7534e5de89923b0479b2c5f709a
Jhin Woo <muojeso90@gmail.com>
2024-02-07 16:46:04 +0900

    SpringBuildFiles

---
+++ src/main/java/com/teamsix/firstteamproject/FirstTeamprojectApplication.java
@@ -0,0 +1,13 @@
+package com.teamsix.firstteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTeamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTeamprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamsix/firstteamproject/FirstTeamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsix.firstteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTeamprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
